0b3ccbde7ef6b7469c58ae1cb7f2e7cf612b6d53
uglo1 <yujiro.takahashi0912@gmail.com>
2022-01-02 16:36:38 +0900

    記事の内容を表示

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
@@ -10,7 +11,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -21,11 +22,17 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
       {postData.title}
       <br />
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
